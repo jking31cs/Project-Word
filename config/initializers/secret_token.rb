b92d079f77d80e787e5e1596ce0ebb80901d112a
James King <jamesking@lawn-143-215-115-239.lawn.gatech.edu>
2011-12-15 22:42:59 -0500

    Created the Rails Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectWord::Application.config.secret_token = '53641fe25cae8aa7dfee0be4dda654ee3c3ba905763a53d092fa0f9d2eaaa6cba1e655bf47d3c544b92aae740cb0c2ee42306b4ca834a47b5f5a5d2188d9c2e8'
