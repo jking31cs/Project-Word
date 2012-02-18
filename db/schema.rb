195b47f805c1e715e8eb632f5465fbbb1526f14d
James King <jking31cs@gmail.com>
2012-02-18 15:33:27 -0500

    Created models and migration scripts for all objects

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,12 +11,52 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111215010410) do
+ActiveRecord::Schema.define(:version => 20120218200718) do
 
   create_table "attachments", :force => true do |t|
     t.string "filename"
     t.string "content_type"
     t.binary "data"
+  end
+
+  create_table "courses", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "docs", :force => true do |t|
+    t.integer  "course_id"
+    t.date     "datetime"
+    t.integer  "type_id"
+    t.integer  "semester_id"
+    t.integer  "owner_id"
+    t.string   "prof"
+    t.binary   "data"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "people", :force => true do |t|
+    t.string   "name"
+    t.string   "major"
+    t.integer  "gradYear"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "semesters", :force => true do |t|
+    t.integer  "year"
+    t.string   "season"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "types", :force => true do |t|
+    t.string   "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
