818ac6c5b5ee57f347df14700e52939a8f9b29e2
James King <jking31cs@gmail.com>
2012-02-18 17:34:43 -0500

    MOTHER FUCKING SHIT IT KINDA WORKS

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -2,6 +2,7 @@ class DocsController < ApplicationController
   def new
   end
   def index
+     @docs = Doc.all
   end
   def create      
         return if params[:file].blank?
@@ -16,5 +17,9 @@ class DocsController < ApplicationController
             flash[:error] = "There was a problem submitting your attachment."
             render :action => "new"
       end
+  end
+  def show
+    @doc = Doc.find(params[:id])
+    send_data @doc.data, :filename => @doc.title
   end
 end
